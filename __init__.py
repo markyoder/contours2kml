6feab03665cf83751d9a78111bf3f1dc8d136247
mark.yoder <mark.yoder@gmail.com>
2017-03-02 21:04:43 -0800

    tweaking __init__.py to make package work.

--- __init__.py
+++ __init__.py
@@ -5,8 +5,12 @@ import imp
 from contours2kml import *
 
 excluded_py_files=['__init__.py']
-local_path = os.path.split(__file__)[0]
+#local_path = os.path.split(__file__)[0]
+local_path = os.path.split(sys.modules[__name__].__file__)[0]
 
+if not local_path in sys.path:
+	sys.path.append(local_path)
+	
 __all__ = [os.path.splitext(os.path.split(g)[-1])[0] for g in glob.glob(os.path.join(local_path,'*.py')) if not os.path.split(g)[-1] in excluded_py_files]
 
 '''
